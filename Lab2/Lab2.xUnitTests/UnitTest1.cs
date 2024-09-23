3787fe8c9b1b6e4ad170effae87a4ca2fda59b97
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-09-23 23:57:13 +0300

    Added

---
+++ Lab2/Lab2.xUnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Lab2.xUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            string p1 = "???";
+            string p2 = "abc";
+            int expected = 64;
+
+            // Act
+            //int actual = Program.GetResult(p1, p2);
+
+            // Assert
+            //Assert.Equal(expected, actual);
+        }
+    }
+}
