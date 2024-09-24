af6bc2cb12c976639036a522481b2749484a374d
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-09-24 21:48:31 +0300

    Added

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(File.ReadAllText("INPUT.TXT"));
+
+            List<int> list = new List<int>() { 1 };
+
+            int current = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                current = list[0];
+                list.RemoveAt(0);
+                AddToList(list, current * 2);
+                AddToList(list, current * 3);
+                AddToList(list, current * 5);
+
+                list.Sort();
+            }
+
+            File.WriteAllText("OUTPUT.TXT", current.ToString());
+        }
+
+        static void AddToList(List<int> list, int value)
+        {
+            if (!list.Contains(value))
+            {
+                list.Add(value);
+            }
         }
     }
 }
