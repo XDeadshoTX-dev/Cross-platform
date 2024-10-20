2792ad83d534bf2737a163b37f082ab800d8571e
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-20 18:30:43 +0300

    Added class for 1, 2, 3 labs

---
+++ Lab5/Lab5/Controllers/LabsLibrary/LabsLibrary.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Lab4
+{
+    public class LabsLibrary
+    {
+
+        public static void RunCommand(string command)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                ExecuteCommand("cmd.exe", $"/c {command}");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                ExecuteCommand("/bin/bash", $"-c \"{command}\"");
+            }
+            else
+            {
+                Console.WriteLine("Unsupported operating system");
+            }
+        }
+
+        private static void ExecuteCommand(string shell, string arguments)
+        {
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = shell,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var process = Process.Start(processInfo))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                Console.WriteLine(output);
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Console.WriteLine($"Error: {error}");
+                }
+            }
+        }
+        public class Lab1
+        {
+            public void Build()
+            {
+                Console.WriteLine("Building Lab1...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab1 -t:Build");
+            }
+            public void Test()
+            {
+                Console.WriteLine("Testing Lab1...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab1 -t:Test");
+            }
+            public void Run(string inputFile, string outputFile)
+            {
+                Console.WriteLine("Starting Lab1...");
+
+                string destinationFilePath = "../../Lab1/INPUT.TXT";
+                File.Copy(inputFile, destinationFilePath, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {destinationFilePath}");
+
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab1 -t:Run");
+
+                string resultFilePath = "../../Lab1/OUTPUT.TXT";
+                File.Copy(resultFilePath, outputFile, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {outputFile}");
+            }
+        }
+
+        public class Lab2
+        {
+            public void Build()
+            {
+                Console.WriteLine("Building Lab2...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab2 -t:Build");
+            }
+            public void Test()
+            {
+                Console.WriteLine("Testing Lab2...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab2 -t:Test");
+            }
+            public void Run(string inputFile, string outputFile)
+            {
+                Console.WriteLine("Starting Lab2...");
+
+                string destinationFilePath = "../../Lab2/INPUT.TXT";
+                File.Copy(inputFile, destinationFilePath, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {destinationFilePath}");
+
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab2 -t:Run");
+
+                string resultFilePath = "../../Lab2/OUTPUT.TXT";
+                File.Copy(resultFilePath, outputFile, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {outputFile}");
+            }
+        }
+
+        public class Lab3
+        {
+            public void Build()
+            {
+                Console.WriteLine("Building Lab3...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab3 -t:Build");
+            }
+            public void Test()
+            {
+                Console.WriteLine("Testing Lab3...");
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab3 -t:Test");
+            }
+            public void Run(string inputFile, string outputFile)
+            {
+                Console.WriteLine("Starting Lab3...");
+
+                string destinationFilePath = "../../Lab3/INPUT.TXT";
+                File.Copy(inputFile, destinationFilePath, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {destinationFilePath}");
+
+                RunCommand("dotnet build ../../Build.proj -p:Solution=Lab3 -t:Run");
+
+                string resultFilePath = "../../Lab3/OUTPUT.TXT";
+                File.Copy(resultFilePath, outputFile, overwrite: true);
+                Console.WriteLine($"File copied successfully to: {outputFile}");
+            }
+        }
+    }
+}
