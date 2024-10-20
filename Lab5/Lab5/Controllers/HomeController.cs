3f40f45b551b1c840e964c78e651e64842689691
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-20 14:12:22 +0300

    Added GetClientToken, CreateUser

--- Lab5/Lab5/Controllers/HomeController.cs
+++ Lab5/Lab5/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab5.Controllers
 {
@@ -9,16 +15,93 @@ namespace Lab5.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public IActionResult RegistrationAuth0(string username, string fullname, string password, string passwordConfirm, string phone, string email)
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private static readonly string _auth0Domain = Environment.GetEnvironmentVariable("Auth0Domain");
+        private readonly string _clientId = Environment.GetEnvironmentVariable("ClientID");
+        private readonly string _clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+        private readonly string _audience = $"https://{_auth0Domain}/api/v2/";
+        private class TokenResponse
         {
-            if(password != passwordConfirm)
+            //[JsonProperty("access_token")]
+            public string access_token { get; set; }
+        }
+        public async Task<string> GetClientTokenAsync()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"https://{_auth0Domain}/oauth/token");
+            request.Content = new StringContent(JsonConvert.SerializeObject(new
             {
-                ViewBag.Error = "Паролі не збігаються!";
+                audience = _audience,
+                grant_type = "client_credentials",
+                client_id = _clientId,
+                client_secret = _clientSecret
+            }), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error getting client token");
+            }
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(jsonResponse);
+            return tokenResponse.access_token;
+        }
+        private async Task CreateUser(string username, string fullname, string password, string phone, string email, string clientToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, $"https://{_auth0Domain}/api/v2/users");
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", clientToken);
+
+            var jsonContent = new
+            {
+                email = email,
+                user_metadata = new { }, 
+                blocked = false,
+                email_verified = false,
+                app_metadata = new { },
+                given_name = "string",
+                family_name = "string",
+                name = fullname,
+                nickname = "string",
+                picture = "https://crazybox.com.ua/image/catalog/exotic_fruits/ananas_gold.JPG",
+                connection = "Username-Password-Authentication",
+                password = password,
+                verify_email = false,
+                username = username
+            };
+
+            request.Content = new StringContent(
+                JsonConvert.SerializeObject(jsonContent),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorResponse = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error creating user: {errorResponse}");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegistrationAuth0(string username, string fullname, string password, string passwordConfirm, string phone, string email)
+        {
+            try
+            {
+                if (password != passwordConfirm)
+                {
+                    ViewBag.Error = "Паролі не збігаються!";
+                    return View("Index");
+                }
+                string clientToken = await GetClientTokenAsync();
+                await CreateUser(username, fullname, password, phone, email, clientToken);
+                ViewBag.Message = "Користувача успішно створено!";
                 return View("Index");
             }
-            return View("Index");
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View("Index");
+            }
         }
     }
 }
