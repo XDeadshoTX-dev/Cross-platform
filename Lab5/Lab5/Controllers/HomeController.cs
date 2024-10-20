4b0a7c8b74d5d2808773f57a212b0114491af443
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-20 17:25:00 +0300

    Edited

--- Lab5/Lab5/Controllers/HomeController.cs
+++ Lab5/Lab5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab5.Controllers.Managements;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -15,96 +16,7 @@ namespace Lab5.Controllers
         {
             return View();
         }
-        private static readonly HttpClient _httpClient = new HttpClient();
-        private static readonly string _auth0Domain = Environment.GetEnvironmentVariable("Auth0Domain");
-        private readonly string _clientId = Environment.GetEnvironmentVariable("ClientID");
-        private readonly string _clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
-        private readonly string _audience = $"https://{_auth0Domain}/api/v2/";
-        private class TokenResponse
-        {
-            //[JsonProperty("access_token")]
-            public string access_token { get; set; }
-        }
-        public async Task<string> GetClientTokenAsync()
-        {
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"https://{_auth0Domain}/oauth/token");
-            request.Content = new StringContent(JsonConvert.SerializeObject(new
-            {
-                audience = _audience,
-                grant_type = "client_credentials",
-                client_id = _clientId,
-                client_secret = _clientSecret
-            }), Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Error getting client token");
-            }
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(jsonResponse);
-            return tokenResponse.access_token;
-        }
-        public async Task<string> GetUserTokenAsync(string username, string password)
-        {
-            var requestData = new FormUrlEncodedContent(new[]
-                {
-                    new KeyValuePair<string, string>("grant_type", "password"),
-                    new KeyValuePair<string, string>("client_id", _clientId),
-                    new KeyValuePair<string, string>("client_secret", _clientSecret),
-                    new KeyValuePair<string, string>("username", username),
-                    new KeyValuePair<string, string>("password", password),
-                    new KeyValuePair<string, string>("audience", _audience),
-                    new KeyValuePair<string, string>("scope", "offline_access"),
-                    new KeyValuePair<string, string>("connection", "Username-Password-Authentication")
-                });
-
-            // Отправляем запрос
-            HttpResponseMessage response = await _httpClient.PostAsync($"https://{_auth0Domain}/oauth/token", requestData);
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"Error getting user token: {response.ToString()}");
-            }
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(jsonResponse);
-            return tokenResponse.access_token;
-        }
-        private async Task CreateUserAsync(string username, string fullname, string password, string phone, string email, string clientToken)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, $"https://{_auth0Domain}/api/v2/users");
-
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", clientToken);
-
-            var jsonContent = new
-            {
-                email = email,
-                user_metadata = new { }, 
-                blocked = false,
-                email_verified = false,
-                app_metadata = new { },
-                given_name = "string",
-                family_name = "string",
-                name = fullname,
-                nickname = "string",
-                connection = "Username-Password-Authentication",
-                password = password,
-                verify_email = false,
-                username = username
-            };
-
-            request.Content = new StringContent(
-                JsonConvert.SerializeObject(jsonContent),
-                Encoding.UTF8,
-                "application/json"
-            );
-
-            var response = await _httpClient.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                var errorResponse = await response.Content.ReadAsStringAsync();
-                throw new Exception($"Error creating user: {errorResponse}");
-            }
-        }
+        AuthManagements authManagements = new AuthManagements();
         [HttpPost]
         public async Task<IActionResult> RegistrationAuth0(string username, string fullname, string password, string passwordConfirm, string phone, string email)
         {
@@ -115,8 +27,8 @@ namespace Lab5.Controllers
                     ViewBag.Error = "Паролі не збігаються!";
                     return View("Index");
                 }
-                string clientToken = await GetClientTokenAsync();
-                await CreateUserAsync(username, fullname, password, phone, email, clientToken);
+                string clientToken = await authManagements.GetClientTokenAsync();
+                await authManagements.CreateUserAsync(username, fullname, password, phone, email, clientToken);
                 ViewBag.Message = "Користувача успішно створено!";
                 return View("Index");
             }
@@ -135,7 +47,7 @@ namespace Lab5.Controllers
                     ViewBag.Error = "Введіть логін та пароль!";
                     return View("Index");
                 }
-                string userToken = await GetUserTokenAsync(username, password);
+                string userToken = await authManagements.GetUserTokenAsync(username, password);
                 return View("Control");
             }
             catch(Exception ex) 
