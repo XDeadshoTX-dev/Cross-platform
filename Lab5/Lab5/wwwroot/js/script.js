527d92da3afe95edfdf7ccd68016f0a85502b388
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-23 13:59:22 +0300

    Added

---
+++ Lab5/Lab5/wwwroot/js/script.js
@@ -0,0 +1,24 @@
+﻿const sendButton = document.getElementById('sendButton');
+sendButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const inputFile = document.getElementById('inputFile').files[0];
+    const lab = document.getElementById('lab').value;
+
+    const formData = new FormData();
+    formData.append('inputFile', inputFile);
+    formData.append('lab', lab);
+
+    try {
+
+        const response = await axios.post('/Home/StartLab', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+
+        console.log(response.data);
+    } catch (error) {
+        console.log('Error:', error);
+    }
+});
