5fc035a7ec5b92465bbd6b07707e47dff3832e2d
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-23 13:49:00 +0300

    Adapted for frontend

--- Lab5/Lab5/Controllers/LabsLibrary/LabsLibrary.cs
+++ Lab5/Lab5/Controllers/LabsLibrary/LabsLibrary.cs
@@ -12,27 +12,37 @@ namespace Lab4
     public class LabsLibrary
     {
         private string lab;
+        private string output = "";
+        public string GetOutputConsole
+        {
+            get
+            {
+                return output;
+            }
+        }
         public LabsLibrary(string lab)
         {
             this.lab = lab;
         }
-        private static void RunCommand(string command)
+        private static string RunCommand(string command)
         {
+            string outputExecute;
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                ExecuteCommand("cmd.exe", $"/c {command}");
+                outputExecute = ExecuteCommand("cmd.exe", $"/c {command}");
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                ExecuteCommand("/bin/bash", $"-c \"{command}\"");
+                outputExecute = ExecuteCommand("/bin/bash", $"-c \"{command}\"");
             }
             else
             {
-                Console.WriteLine("Unsupported operating system");
+                outputExecute = "Unsupported operating system";
             }
+            return outputExecute;
         }
 
-        private static void ExecuteCommand(string shell, string arguments)
+        private static string ExecuteCommand(string shell, string arguments)
         {
             var processInfo = new ProcessStartInfo
             {
@@ -43,11 +53,11 @@ namespace Lab4
                 UseShellExecute = false,
                 CreateNoWindow = true
             };
-
+            string output, error;
             using (var process = Process.Start(processInfo))
             {
-                string output = process.StandardOutput.ReadToEnd();
-                string error = process.StandardError.ReadToEnd();
+                output = process.StandardOutput.ReadToEnd();
+                error = process.StandardError.ReadToEnd();
                 process.WaitForExit();
 
                 Console.WriteLine(output);
@@ -57,22 +67,28 @@ namespace Lab4
                     Console.WriteLine($"Error: {error}");
                 }
             }
+            return output;
         }
         public void Build()
         {
-            Console.WriteLine($"Building {lab}...");
-            RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Build");
+            string text = $"Building {lab}...\n";
+            Console.WriteLine(text);
+            output += text;
+            output += RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Build");
         }
         public void Test()
         {
-            Console.WriteLine($"Testing {lab}...");
-            RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Test");
+            string text = $"Testing {lab}...\n";
+            Console.WriteLine(text);
+            output += text;
+            output += RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Test");
         }
         public void Run()
         {
-            Console.WriteLine($"Starting {lab}...");
-
-            RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Run");
+            string text = $"Starting {lab}...\n";
+            Console.WriteLine(text);
+            output += text;
+            output += RunCommand($"dotnet build ../../Build.proj -p:Solution={lab} -t:Run");
         }
     }
 }
