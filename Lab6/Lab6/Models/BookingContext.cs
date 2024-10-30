9f427d3b713c125d93c05a2eee1a9a15ee4250a5
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-30 16:31:13 +0200

    Added context

--- Lab6/Lab6/Models/BookingContext.cs
+++ Lab6/Lab6/Models/BookingContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Lab6.Models
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Lab6.Models
 {
-    public class BookingContext
+    public class BookingContext : DbContext
     {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleCategory> VehicleCategories { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<BookingStatus> BookingStatuses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        private readonly string _connectionString = @"Server=(localdb)\mssqllocaldb;Database=Lab6;Trusted_Connection=True;ConnectRetryCount=0";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
     }
 }
