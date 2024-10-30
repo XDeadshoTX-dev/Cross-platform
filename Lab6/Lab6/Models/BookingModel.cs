aa521fc95766baac8db2087dbc6ebe133b9c8e7b
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-10-30 16:31:18 +0200

    Added model

--- Lab6/Lab6/Models/BookingModel.cs
+++ Lab6/Lab6/Models/BookingModel.cs
@@ -1,68 +1,155 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab6.Models
 {
-    public class BookingModel
+    public class Booking
     {
-        public class Booking
-        {
-            public int booking_id { get; set; }
-            public string booking_status_code { get; set; }
-            public int customer_id { get; set; }
-            public int reg_number { get; set; }
-            public DateTime date_from { get; set; }
-            public DateTime date_to { get; set; }
-            public string confirmation_letter_sent_yn { get; set; }
-            public string payment_received_yn { get; set; }
-        }
-        public class Vehicle
-        {
-            public string reg_number { get; set; }
-            public string manufacturer_code { get; set; }
-            public string model_code { get; set; }
-            public string vehicle_category_code { get; set; }
-            public int current_mileage { get; set; }
-            public decimal daily_hire_rate { get; set; }
-            public DateTime date_mot_due { get; set; }
-        }
-        public class VehicleCategory
-        {
-            public string vehicle_category_code { get; set; }
-            public string vehicle_category_description { get; set; }
-        }
-        public class Model
-        {
-            public string model_code { get; set; }
-            public decimal daily_hire_rate { get; set; }
-            public string model_name { get; set; }
-        }
-        public class Manufacturer
-        {
-            public string manufacturer_code { get; set; }
-            public string manufacturer_name { get; set; }
-            public string manufacturer_details { get; set; }
-        }
-        public class BookingStatus
-        {
-            public string booking_status_code { get; set; }
-            public string booking_status_description { get; set; }
-        }
-        public class Customer
-        {
-            public string customer_id { get; set; }
-            public string customer_name { get; set; }
-            public string customer_details { get; set; }
-            public string gender { get; set; }
-            public string email_address { get; set; }
-            public string phone_number { get; set; }
-            public string address_line_1 { get; set; }
-            public string address_line_2 { get; set; }
-            public string address_line_3 { get; set; }
-            public string town { get; set; }
-            public string county { get; set; }
-            public string country { get; set; }
-        }
+        [Key]
+        public int booking_id { get; set; }
+
+        [Column(TypeName = "char(3)")]
+        public string booking_status_code { get; set; }
+        public virtual BookingStatus BookingStatus { get; set; }
+
+        public int customer_id { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        public string reg_number { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime date_from { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime date_to { get; set; }
+
+        [Column(TypeName = "char(1)")]
+        public string confirmation_letter_sent_yn { get; set; }
+
+        [Column(TypeName = "char(1)")]
+        public string payment_received_yn { get; set; }
+    }
+    public class Vehicle
+    {
+        [Key]
+        [Column(TypeName = "char(10)")]
+        public string reg_number { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        public string manufacturer_code { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; }
+
+
+        [Column(TypeName = "char(10)")]
+        public string model_code { get; set; }
+        public virtual Model Model { get; set; }
+
+        [Column(TypeName = "char(5)")]
+        public string vehicle_category_code { get; set; }
+        public virtual VehicleCategory VehicleCategory { get; set; }
+
+        public int current_mileage { get; set; }
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal daily_hire_rate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime date_mot_due { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+    public class VehicleCategory
+    {
+        [Key]
+        [Column(TypeName = "char(5)")]
+        public string vehicle_category_code { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        public string vehicle_category_description { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+    public class Model
+    {
+        [Key]
+        [Column(TypeName = "char(10)")]
+        public string model_code { get; set; }
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal daily_hire_rate { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string model_name { get; set; }
+
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+    public class Manufacturer
+    {
+        [Key]
+        [Column(TypeName = "char(10)")]
+        public string manufacturer_code { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string manufacturer_name { get; set; }
+
+        [Column(TypeName = "varchar(2000)")]
+        public string manufacturer_details { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+
+    }
+    public class BookingStatus
+    {
+        [Key]
+        [Column(TypeName = "char(3)")]
+        public string booking_status_code { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        public string booking_status_description { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+
+    }
+    public class Customer
+    {
+        [Key]
+        public int customer_id { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string customer_name { get; set; }
+
+        [Column(TypeName = "varchar(2000)")]
+        public string customer_details { get; set; }
+
+        [Column(TypeName = "char(1)")]
+        public string gender { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string email_address { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string phone_number { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string address_line_1 { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string address_line_2 { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string address_line_3 { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string town { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string county { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string country { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+
+
     }
 }
