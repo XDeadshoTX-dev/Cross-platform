06e251153a3a321ce0402058da12380946e18279
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-11-16 20:35:55 +0200

    Added Salesforce page

---
+++ Lab6/Lab6/Pages/Salesforce.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab6.Pages
+{
+    public class SalesforceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
