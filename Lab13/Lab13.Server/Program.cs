d666a97b89ed38ed092cf5d2e0e7a329b2fc74ca
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-11-17 16:56:24 +0200

    Added support DotNetEnv

--- Lab13/Lab13.Server/Program.cs
+++ Lab13/Lab13.Server/Program.cs
@@ -1,4 +1,6 @@
 
+using Lab13.Server.Controllers.Managements;
+
 namespace Lab13.Server
 {
     public class Program
@@ -8,6 +10,7 @@ namespace Lab13.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            DotNetEnv.Env.Load();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
