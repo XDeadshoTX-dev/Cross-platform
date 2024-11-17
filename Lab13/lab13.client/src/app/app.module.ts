1d557a84c11f7717358c9ed06d6113f69f22d253
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-11-17 19:00:05 +0200

    add new modules (fixed bind to 'ngModel')

--- Lab13/lab13.client/src/app/app.module.ts
+++ Lab13/lab13.client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,9 @@ import { ControlComponent } from './control/control.component';
     ControlComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
