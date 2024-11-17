c279fa3eb4fc202c98ed845e178117b2baac98d4
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-11-17 18:59:24 +0200

    Fixed api controller

--- Lab13/lab13.client/src/app/login/login.component.ts
+++ Lab13/lab13.client/src/app/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  onSubmitLogin() {
+    const loginData = { username: this.username, password: this.password };
+    console.log('Login data', loginData);
+    this.http.post('http://localhost:5278/api/Home/LoginAuth0', loginData)
+      .subscribe(
+        response => {
+          console.log('Login successful', response);
+        },
+        error => {
+          console.error('Login failed', error);
+        }
+      );
+  }
 }
